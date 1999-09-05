9fb19bcd5ffc0ca283a1f7edc2891bd7aa8efa28
jlewis <unknown>
1999-09-05 14:46:58 +0000

    Fix `type'-o in Stable.hs.

--- lib/exts/Stable.hs
+++ lib/exts/Stable.hs
@@ -5,7 +5,7 @@ data StableName a -- abstract
 primitive makeStableName   :: a -> IO (StableName a)
 primitive deRefStableName  :: StableName a -> a
 primitive hashStableName   :: StableName a -> Int
-primitive eqStableName	   :: StableName a -> StableName a -> Int
+primitive eqStableName	   :: StableName a -> StableName a -> Bool
 
 instance Eq (StableName a) where
     (==) = eqStableName
