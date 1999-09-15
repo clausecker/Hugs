ba242af242e3330d009dd5278c7407af12b86de0
andy <unknown>
1999-09-15 20:06:45 +0000

    More header modifications of license message.

--- src/msc/options.h
+++ src/msc/options.h
@@ -2,14 +2,15 @@
 /* --------------------------------------------------------------------------
  * Configuration options
  *
- * Hugs 98 is Copyright (c) Mark P Jones, Alastair Reid and the Yale
- * Haskell Group 1994-99, and is distributed as Open Source software
- * under the Artistic License; see the file "Artistic" that is included
- * in the distribution for details.
+ * The Hugs 98 system is Copyright (c) Mark P Jones, Alastair Reid, the
+ * Yale Haskell Group, and the Oregon Graduate Institute of Science and
+ * Technology, 1994-1999, All rights reserved.  It is distributed as
+ * free software under the license in the file "License", which is
+ * included in the distribution.
  *
  * $RCSfile: options.h,v $
- * $Revision: 1.3 $
- * $Date: 1999/09/10 22:57:50 $
+ * $Revision: 1.4 $
+ * $Date: 1999/09/15 20:06:45 $
  * ------------------------------------------------------------------------*/
 
 /* --------------------------------------------------------------------------
