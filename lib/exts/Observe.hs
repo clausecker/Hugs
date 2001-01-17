1624d59dc6eb89e97cd723d33528f338ebfac74b
nordland <unknown>
2001-01-17 17:44:23 +0000

    Added missing debugging library.

---
+++ lib/exts/Observe.hs
@@ -0,0 +1,9 @@
+module Observe (observe
+               , bkpt
+	       , setBkpt
+	       ) where
+
+primitive observe :: String -> a -> a
+primitive bkpt    :: String -> a -> a
+primitive setBkpt :: String -> Bool -> IO ()
+
