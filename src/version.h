ea6eefa12ff4980c5f0e8208d545b612c25921ca
lewie <unknown>
2001-12-13 18:29:33 +0000

    Turn off `major version' until we're really ready to ship.

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------*/
 
 /* Is this a major release or not? */
-#define MAJOR_RELEASE 1
+#define MAJOR_RELEASE 0
 
 /* Define this as a string (of _max_ 14 characters) uniquely identifying the 
  * current version. Upper limit of 14 chars is there to make the banner
