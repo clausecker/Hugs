3c88d440bb57a11bbd099b6592690f4aebd420ae
reid <unknown>
2002-06-18 20:28:24 +0000

    Replaced libspec "[Storable_aux.o]" with "[Storable_aux.c]"

--- lib/exts/Storable.hs
+++ lib/exts/Storable.hs
@@ -53,57 +53,57 @@ instance Storable Word16        where { sizeOf _ = szWord16   ; alignment _ = 1;
 instance Storable Word32        where { sizeOf _ = szWord32   ; alignment _ = 1; peekElemOff = rdWord32   ; pokeElemOff = wrWord32    }
 instance Storable Word64        where { sizeOf _ = szWord64   ; alignment _ = 1; peekElemOff = rdWord64   ; pokeElemOff = wrWord64    }
 
-foreign import ccall unsafe "[Storable_aux.o]" szInt       :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szChar      :: Int
--- foreign import ccall unsafe "[Storable_aux.o]" szWideChar  :: Int
--- foreign import ccall unsafe "[Storable_aux.o]" szWord      :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szPtr       :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szFunPtr    :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szFloat     :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szDouble    :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szStablePtr :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szInt8      :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szInt16     :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szInt32     :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szInt64     :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szWord8     :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szWord16    :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szWord32    :: Int
-foreign import ccall unsafe "[Storable_aux.o]" szWord64    :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szInt       :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szChar      :: Int
+-- foreign import ccall unsafe "[Storable_aux.c]" szWideChar  :: Int
+-- foreign import ccall unsafe "[Storable_aux.c]" szWord      :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szPtr       :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szFunPtr    :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szFloat     :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szDouble    :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szStablePtr :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szInt8      :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szInt16     :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szInt32     :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szInt64     :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szWord8     :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szWord16    :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szWord32    :: Int
+foreign import ccall unsafe "[Storable_aux.c]" szWord64    :: Int
 
-foreign import ccall unsafe "[Storable_aux.o]" rdInt       :: Ptr Int           -> Int -> IO Int
-foreign import ccall unsafe "[Storable_aux.o]" rdChar      :: Ptr Char          -> Int -> IO Char
--- foreign import ccall unsafe "[Storable_aux.o]" rdWideChar  :: Ptr Char          -> Int -> IO Char
--- foreign import ccall unsafe "[Storable_aux.o]" rdWord      :: Ptr Word          -> Int -> IO Word
-foreign import ccall unsafe "[Storable_aux.o]" rdPtr       :: Ptr (Ptr a)       -> Int -> IO (Ptr a)
-foreign import ccall unsafe "[Storable_aux.o]" rdFunPtr    :: Ptr (FunPtr a)    -> Int -> IO (FunPtr a)
-foreign import ccall unsafe "[Storable_aux.o]" rdFloat     :: Ptr Float         -> Int -> IO Float
-foreign import ccall unsafe "[Storable_aux.o]" rdDouble    :: Ptr Double        -> Int -> IO Double
-foreign import ccall unsafe "[Storable_aux.o]" rdStablePtr :: Ptr (StablePtr a) -> Int -> IO (StablePtr a)
-foreign import ccall unsafe "[Storable_aux.o]" rdInt8      :: Ptr Int8          -> Int -> IO Int8
-foreign import ccall unsafe "[Storable_aux.o]" rdInt16     :: Ptr Int16         -> Int -> IO Int16
-foreign import ccall unsafe "[Storable_aux.o]" rdInt32     :: Ptr Int32         -> Int -> IO Int32
-foreign import ccall unsafe "[Storable_aux.o]" rdInt64     :: Ptr Int64         -> Int -> IO Int64
-foreign import ccall unsafe "[Storable_aux.o]" rdWord8     :: Ptr Word8         -> Int -> IO Word8
-foreign import ccall unsafe "[Storable_aux.o]" rdWord16    :: Ptr Word16        -> Int -> IO Word16
-foreign import ccall unsafe "[Storable_aux.o]" rdWord32    :: Ptr Word32        -> Int -> IO Word32
-foreign import ccall unsafe "[Storable_aux.o]" rdWord64    :: Ptr Word64        -> Int -> IO Word64
+foreign import ccall unsafe "[Storable_aux.c]" rdInt       :: Ptr Int           -> Int -> IO Int
+foreign import ccall unsafe "[Storable_aux.c]" rdChar      :: Ptr Char          -> Int -> IO Char
+-- foreign import ccall unsafe "[Storable_aux.c]" rdWideChar  :: Ptr Char          -> Int -> IO Char
+-- foreign import ccall unsafe "[Storable_aux.c]" rdWord      :: Ptr Word          -> Int -> IO Word
+foreign import ccall unsafe "[Storable_aux.c]" rdPtr       :: Ptr (Ptr a)       -> Int -> IO (Ptr a)
+foreign import ccall unsafe "[Storable_aux.c]" rdFunPtr    :: Ptr (FunPtr a)    -> Int -> IO (FunPtr a)
+foreign import ccall unsafe "[Storable_aux.c]" rdFloat     :: Ptr Float         -> Int -> IO Float
+foreign import ccall unsafe "[Storable_aux.c]" rdDouble    :: Ptr Double        -> Int -> IO Double
+foreign import ccall unsafe "[Storable_aux.c]" rdStablePtr :: Ptr (StablePtr a) -> Int -> IO (StablePtr a)
+foreign import ccall unsafe "[Storable_aux.c]" rdInt8      :: Ptr Int8          -> Int -> IO Int8
+foreign import ccall unsafe "[Storable_aux.c]" rdInt16     :: Ptr Int16         -> Int -> IO Int16
+foreign import ccall unsafe "[Storable_aux.c]" rdInt32     :: Ptr Int32         -> Int -> IO Int32
+foreign import ccall unsafe "[Storable_aux.c]" rdInt64     :: Ptr Int64         -> Int -> IO Int64
+foreign import ccall unsafe "[Storable_aux.c]" rdWord8     :: Ptr Word8         -> Int -> IO Word8
+foreign import ccall unsafe "[Storable_aux.c]" rdWord16    :: Ptr Word16        -> Int -> IO Word16
+foreign import ccall unsafe "[Storable_aux.c]" rdWord32    :: Ptr Word32        -> Int -> IO Word32
+foreign import ccall unsafe "[Storable_aux.c]" rdWord64    :: Ptr Word64        -> Int -> IO Word64
 
-foreign import ccall unsafe "[Storable_aux.o]" wrInt       :: Ptr Int           -> Int -> Int         -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrChar      :: Ptr Char          -> Int -> Char        -> IO ()
--- foreign import ccall unsafe "[Storable_aux.o]" wrWideChar  :: Ptr Char          -> Int -> Char        -> IO ()
--- foreign import ccall unsafe "[Storable_aux.o]" wrWord      :: Ptr Word          -> Int -> Word        -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrPtr       :: Ptr (Ptr a)       -> Int -> Ptr a       -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrFunPtr    :: Ptr (FunPtr a)    -> Int -> FunPtr a    -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrFloat     :: Ptr Float         -> Int -> Float       -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrDouble    :: Ptr Double        -> Int -> Double      -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrStablePtr :: Ptr (StablePtr a) -> Int -> StablePtr a -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrInt8      :: Ptr Int8          -> Int -> Int8        -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrInt16     :: Ptr Int16         -> Int -> Int16       -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrInt32     :: Ptr Int32         -> Int -> Int32       -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrInt64     :: Ptr Int64         -> Int -> Int64       -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrWord8     :: Ptr Word8         -> Int -> Word8       -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrWord16    :: Ptr Word16        -> Int -> Word16      -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrWord32    :: Ptr Word32        -> Int -> Word32      -> IO ()
-foreign import ccall unsafe "[Storable_aux.o]" wrWord64    :: Ptr Word64        -> Int -> Word64      -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrInt       :: Ptr Int           -> Int -> Int         -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrChar      :: Ptr Char          -> Int -> Char        -> IO ()
+-- foreign import ccall unsafe "[Storable_aux.c]" wrWideChar  :: Ptr Char          -> Int -> Char        -> IO ()
+-- foreign import ccall unsafe "[Storable_aux.c]" wrWord      :: Ptr Word          -> Int -> Word        -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrPtr       :: Ptr (Ptr a)       -> Int -> Ptr a       -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrFunPtr    :: Ptr (FunPtr a)    -> Int -> FunPtr a    -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrFloat     :: Ptr Float         -> Int -> Float       -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrDouble    :: Ptr Double        -> Int -> Double      -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrStablePtr :: Ptr (StablePtr a) -> Int -> StablePtr a -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrInt8      :: Ptr Int8          -> Int -> Int8        -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrInt16     :: Ptr Int16         -> Int -> Int16       -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrInt32     :: Ptr Int32         -> Int -> Int32       -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrInt64     :: Ptr Int64         -> Int -> Int64       -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrWord8     :: Ptr Word8         -> Int -> Word8       -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrWord16    :: Ptr Word16        -> Int -> Word16      -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrWord32    :: Ptr Word32        -> Int -> Word32      -> IO ()
+foreign import ccall unsafe "[Storable_aux.c]" wrWord64    :: Ptr Word64        -> Int -> Word64      -> IO ()
 
