406a32a0d6bbc3f9899204f355deb1494b390036
reid <unknown>
2002-06-21 23:35:49 +0000

    forgeto

---
+++ tests/ffi/types_aux.h
@@ -0,0 +1,17 @@
+
+extern HsInt       iInt       (HsInt       x);
+extern HsChar      iChar      (HsChar      x);
+extern HsAddr      iAddr      (HsAddr      x);
+extern HsPtr       iPtr       (HsPtr       x);
+extern HsFunPtr    iFunPtr    (HsFunPtr    x);
+extern HsFloat     iFloat     (HsFloat     x);
+extern HsDouble    iDouble    (HsDouble    x);
+extern HsStablePtr iStablePtr (HsStablePtr x);
+extern HsInt8      iInt8      (HsInt8      x);
+extern HsInt16     iInt16     (HsInt16     x);
+extern HsInt32     iInt32     (HsInt32     x);
+extern HsInt64     iInt64     (HsInt64     x);
+extern HsWord8     iWord8     (HsWord8     x);
+extern HsWord16    iWord16    (HsWord16    x);
+extern HsWord32    iWord32    (HsWord32    x);
+extern HsWord64    iWord64    (HsWord64    x);
