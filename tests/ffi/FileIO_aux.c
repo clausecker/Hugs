8e0eea36dd2d30d18e1b9d1a9a80d8f30e76d888
reid <unknown>
2002-07-09 10:50:21 +0000

    braino

--- tests/ffi/FileIO_aux.c
+++ tests/ffi/FileIO_aux.c
@@ -4,5 +4,5 @@
 
 int open_for_read(const char* pathname, int flags)
 {
-  return open(pathname,O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU,0777);
+  return open(pathname,O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 }
