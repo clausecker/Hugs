922a7ae705dbbd5b5a5a48409052581b991e4b21
lewie <unknown>
2002-08-23 15:31:10 +0000

    Turn off MAJOR_RELEASE for now.  Also, make major version name also more amenable to sed'ing.

--- src/version.h
+++ src/version.h
@@ -3,19 +3,19 @@
  * ------------------------------------------------------------------------*/
 
 /* Is this a major release or not? */
-#define MAJOR_RELEASE 1
+#define MAJOR_RELEASE 0
 
 /* Define this as a string (of _max_ 14 characters) uniquely identifying the 
  * current version. Upper limit of 14 chars is there to make the banner
  * come out nice and aligned.
  *
  * Major releases are of the form "<month> <year>"
- * Minor releases are of the form "Version YYMMDD"
+ * Minor releases are of the form "Version YYYYMMDD"
  * Anyone else should use a different format to avoid confusion.    
  */
 #if MAJOR_RELEASE
-#define HUGS_VERSION "December 2001"
+#define HUGS_VERSION "MONTH_YEAR"
 #else
-#define HUGS_VERSION "Version YYMMDD"
+#define HUGS_VERSION "Version YYYYMMDD"
 #endif
 
