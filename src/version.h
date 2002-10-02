6c43e7524c97441a63962fce6c8f4155f5bb661e
sof <unknown>
2002-10-02 22:44:57 +0000

    prep for Oct release..

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------*/
 
 /* Is this a major release or not? */
-#define MAJOR_RELEASE 0
+#define MAJOR_RELEASE 1
 
 /* Define this as a string (of _max_ 14 characters) uniquely identifying the 
  * current version. Upper limit of 14 chars is there to make the banner
@@ -14,7 +14,7 @@
  * Anyone else should use a different format to avoid confusion.    
  */
 #if MAJOR_RELEASE
-#define HUGS_VERSION "MONTH_YEAR"
+#define HUGS_VERSION "Oct 2002 RC1"
 #else
 #define HUGS_VERSION "SNAP YYYYMMDD"
 #endif
