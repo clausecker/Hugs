e2c4059d2b2617ea8a2912eeaa1c5235381e79ae
lewie <unknown>
2002-10-20 22:59:21 +0000

    Bring up-to-date.

--- RPM.mk
+++ RPM.mk
@@ -2,6 +2,9 @@
 # somewhat redhat specific (and rpm version 3 specific - i.e. RedHat 6.0
 # or greater).
 
+# need to conditionalize
+RPMBUILD = rpmbuild
+
 RPMTMP = /tmp/rpm
 # probably should be uniqueified
 TARTMP = /tmp/mktar
@@ -23,14 +26,14 @@ ${PACKAGE}.tar.gz:
 	cd ${TARTMP}/hugs98; cvs -d ${CVSROOT} export -r${HSLIBSTAG} $(addprefix fptools/hslibs/,${HSLIBSDIRS})
 	cd ${TARTMP}/hugs98; cvs -d ${CVSROOT} export -r${LIBRARIESTAG} $(addprefix fptools/libraries/,${LIBRARIESDIRS})
 	cd ${TARTMP}/hugs98/fptools/libraries/haskell-src/Language/Haskell; happy Parser.ly
+	cp ${TARTMP}/hugs98/src/version.c /tmp/mktar
+	cd ${TARTMP}/hugs98/src; sed -e "s/define MAJOR_RELEASE.*/define MAJOR_RELEASE 0/" -e "s/VERSION_STRING MONTH_YEAR/VERSION_STRING \"${MONTH_YEAR}\"/" -e "s/VERSION_STRING YYYYMMDD/VERSION_STRING \"${YEAR_MONTH_DAY}\"/" < ${TARTMP}/version.c > ${TARTMP}/hugs98/src/version.c
 	# using `make parser.c' would be best, but by default yacc
 	# will be used, and yacc is, for some reason, incompatible
-	cp ${TARTMP}/hugs98/src/version.h.in /tmp/mktar
-	cd ${TARTMP}/hugs98/src; sed -e "s/MONTH_YEAR/${MONTH_YEAR}/" -e "s/YYYYMMDD/${YEAR_MONTH_DAY}/" < ${TARTMP}/version.h.in > ${TARTMP}/hugs98/src/version.h.in
 	cd ${TARTMP}/hugs98/src; bison -y parser.y; mv y.tab.c parser.c
 	# Siggy deren't like these in distros
 	cd ${TARTMP}/hugs98; rm -rf tests
-	cd ${TARTMP}/hugs98/src/unix; autoconf; autoheader
+	cd ${TARTMP}/hugs98/src/unix; autoconf # ; autoheader
 	mv ${TARTMP}/hugs98 ${TARTMP}/${PACKAGE}
 	cd ${TARTMP}; tar cf /tmp/${NAME}.tar ${PACKAGE}
 	gzip -9 /tmp/${NAME}.tar
@@ -47,7 +50,7 @@ rpm-dirs:
 
 rpm: tar rpm-dirs
 	cp ${PACKAGE}.tar.gz ${RPMTMP}/SOURCES
-	rpm ${RPMDEFS} -ba ${SPECFILE}
+	${RPMBUILD} ${RPMDEFS} -ba ${SPECFILE}
 	mv ${RPMTMP}/RPMS/i386/${PACKAGE}-${RELEASE}.i386.rpm .
 	mv ${RPMTMP}/SRPMS/${PACKAGE}-${RELEASE}.src.rpm .
 
