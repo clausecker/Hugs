7531185db4cb04e136f004273f9d3161421a3ac8
sof <unknown>
2003-03-12 16:41:50 +0000

    - naming wibbles - generate FFI decls for fields also - emit full complement of unboxed prim types (i.e, instead of Dotnet.System.UInt16,   Word16.)

--- dotnet/tools/HsOutput.cs
+++ dotnet/tools/HsOutput.cs
@@ -23,10 +23,10 @@ namespace HsWrapGen
 	}
 
         protected void OutputHeader(System.IO.StreamWriter st) {
-            st.WriteLine("module {0} where", m_type.FullName);
+            st.WriteLine("module Dotnet.{0} where", m_type.FullName);
             st.WriteLine("");
-            st.WriteLine("import DotNet");
-            st.WriteLine("import qualified {0}", m_type.BaseType.FullName);
+            st.WriteLine("import Dotnet");
+            st.WriteLine("import qualified Dotnet.{0}", m_type.BaseType.FullName);
             foreach (String s in m_imports) {
               st.WriteLine("import qualified {0}", s);
             }
@@ -34,7 +34,7 @@ namespace HsWrapGen
 	    // ToDo: provide the option of stashing this away in a separate
 	    //       module.
             st.WriteLine("data {0}_ a", m_type.Name);
-            st.WriteLine("type {0} a = {1}.{2} ({0}_ a)",
+            st.WriteLine("type {0} a = Dotnet.{1}.{2} ({0}_ a)",
                          m_type.Name,
                          m_type.BaseType.FullName,
                          m_type.BaseType.Name);
@@ -91,9 +91,6 @@ namespace HsWrapGen
         protected void OutputHaskellType(System.Text.StringBuilder sb,
                                          System.Type ty,
                                          System.Int32 idx) {
-            if (ty.FullName == "System.Int32") {
-              sb.Append("Int"); return;
-            }
             if (ty.FullName == "System.Boolean") {
               sb.Append("Bool"); return;
             }
@@ -103,21 +100,58 @@ namespace HsWrapGen
             if (ty.FullName == "System.Char") {
               sb.Append("Char"); return;
             }
+            if (ty.FullName == "System.Double") {
+              sb.Append("Double"); return;
+            }
+            if (ty.FullName == "System.Single") {
+              sb.Append("Double"); return;
+            }
+            if (ty.FullName == "System.SByte") {
+	      AddImport("Data.Int");
+              sb.Append("Data.Int.Int8"); return;
+            }
+            if (ty.FullName == "System.Int16") {
+	      AddImport("Data.Int");
+              sb.Append("Data.Int.Int16"); return;
+            }
+            if (ty.FullName == "System.Int32") {
+              sb.Append("Int"); return;
+            }
+            if (ty.FullName == "System.Int64") {
+	      AddImport("Data.Int");
+              sb.Append("Data.Int.Int64"); return;
+            }
+            if (ty.FullName == "System.Byte") {
+	      AddImport("Data.Word");
+              sb.Append("Data.Word.Word8"); return;
+            }
+            if (ty.FullName == "System.UInt16") {
+	      AddImport("Data.Word");
+              sb.Append("Data.Word.Word16"); return;
+            }
+            if (ty.FullName == "System.UInt32") {
+	      AddImport("Data.Word");
+              sb.Append("Data.Word.Word32"); return;
+            }
+            if (ty.FullName == "System.UInt64") {
+	      AddImport("Data.Word");
+              sb.Append("Data.Word.Word64"); return;
+            }
             if (ty.FullName == "System.Void") {
               sb.Append("()"); return;
             }
             if (ty.FullName == "System.Object") {
-                sb.AppendFormat("System.Object.Object a{0}",idx); return;
+                sb.AppendFormat("Dotnet.System.Object.Object a{0}",idx); return;
             }
 
             if (ty.IsArray) {
                 String eltNm = ty.GetElementType().FullName;
-                AddImport("System.Array");
-                AddImport(eltNm);
-                sb.AppendFormat("System.Array.Array ({0}.{1} a{2})", eltNm, ty.GetElementType().Name, idx);
+                AddImport("Dotnet.System.Array");
+                AddImport("Dotnet."+eltNm);
+                sb.AppendFormat("Dotnet.System.Array.Array ({0}.{1} a{2})", eltNm, ty.GetElementType().Name, idx);
             } else {
-                AddImport(ty.FullName);
-                sb.AppendFormat("{0}.{1} a{2}", ty.FullName, ty.Name, idx);
+                AddImport("Dotnet." + ty.FullName);
+                sb.AppendFormat("Dotnet.{0}.{1} a{2}", ty.FullName, ty.Name, idx);
             }       
         }
 
@@ -139,6 +173,28 @@ namespace HsWrapGen
             OutputHaskellType(sb,m.ReturnType,i);
             sb.AppendFormat("){0}",System.Environment.NewLine);
         }
+
+        protected void OutputFieldSig(System.Text.StringBuilder sb,
+				      System.Reflection.FieldInfo fi,
+				      bool isSetter) {
+
+	    /* Note: indexed values are provided via properties */
+	    if (isSetter) {
+	      OutputHaskellType(sb,fi.FieldType,0);
+	      if (!fi.IsStatic) {
+		sb.AppendFormat(" -> {0} obj", fi.DeclaringType.Name);
+	      }
+	      sb.AppendFormat(" -> IO (){0}",System.Environment.NewLine);
+	    } else {
+	      if (fi.IsStatic) {
+		sb.Append("IO (");
+	      } else {
+		sb.AppendFormat("{0} obj -> IO (", fi.DeclaringType.Name);
+	      }
+	      OutputHaskellType(sb,fi.FieldType,0);
+	      sb.AppendFormat("){0}",System.Environment.NewLine);
+	    }
+	}
 
         protected void OutputArgs(System.Text.StringBuilder sb,
                                   System.Reflection.MemberInfo mi,
@@ -163,21 +219,43 @@ namespace HsWrapGen
         protected void OutputMember(System.Text.StringBuilder sb,
 				    System.Reflection.MemberInfo mi) {
             switch (mi.MemberType) {
-                case System.Reflection.MemberTypes.Method:
-                    System.String methName = ToHaskellName(mi.Name);
-		    System.Reflection.MethodInfo m = (System.Reflection.MethodInfo)mi;
-		    sb.Append("foreign import dotnet");
-                    sb.AppendFormat("{0}",System.Environment.NewLine);
-		    // the 'method' bit is really optional.
-		    sb.AppendFormat("  \"{0}method {1}.{2}\"", (m.IsStatic ? "static " : ""), mi.DeclaringType, mi.Name);
-                    sb.AppendFormat("{0}",System.Environment.NewLine);
-		    sb.AppendFormat("  {0} :: ", methName);
-		    OutputMethodSig(sb,mi);
-                    // the mind boggles, System.Environment ?
-                    sb.AppendFormat("{0}",System.Environment.NewLine);
-                    break;
-                default:
-                    break;
+	    case System.Reflection.MemberTypes.Method:
+	      System.String methName = ToHaskellName(mi.Name);
+	      System.Reflection.MethodInfo m = (System.Reflection.MethodInfo)mi;
+	      sb.Append("foreign import dotnet");
+	      sb.AppendFormat("{0}",System.Environment.NewLine);
+	      // the 'method' bit is really optional.
+	      sb.AppendFormat("  \"{0}method {1}.{2}\"", (m.IsStatic ? "static " : ""), mi.DeclaringType, mi.Name);
+	      sb.AppendFormat("{0}",System.Environment.NewLine);
+	      sb.AppendFormat("  {0} :: ", methName);
+	      OutputMethodSig(sb,mi);
+	      // the mind boggles, System.Environment ?
+	      sb.Append(System.Environment.NewLine);
+	      /* old habit ;) */
+	      break;
+	    case System.Reflection.MemberTypes.Field:
+	      System.String fieldName = mi.Name;
+	      System.Reflection.FieldInfo f = (System.Reflection.FieldInfo)mi;
+	      System.String staticPrefix = (f.IsStatic ? "static " : "");
+	      sb.Append("foreign import dotnet");
+	      sb.Append(System.Environment.NewLine);
+	      sb.AppendFormat("  \"{0}field {1}.{2}\"", staticPrefix, mi.DeclaringType, mi.Name);
+	      sb.Append(System.Environment.NewLine);
+	      sb.AppendFormat("  get_{0} :: ", fieldName);
+	      OutputFieldSig(sb,f,false);
+	      sb.Append(System.Environment.NewLine);
+	      if (!f.IsInitOnly) {
+		sb.Append("foreign import dotnet");
+		sb.Append(System.Environment.NewLine);
+		sb.AppendFormat("  \"{0}field {1}.{2}\"", staticPrefix, mi.DeclaringType, mi.Name);
+		sb.Append(System.Environment.NewLine);
+		sb.AppendFormat("  set_{0} :: ", fieldName);
+		OutputFieldSig(sb,f,true);
+		sb.Append(System.Environment.NewLine);
+	      }
+	      break;
+	    default:
+	      break;
             }
         }
         
@@ -212,8 +290,8 @@ namespace HsWrapGen
 	      sb.AppendFormat("  deriving ( Enum, Show, Read ){0}",System.Environment.NewLine);
 	      // Emit functions for converting betw alg type and object type.
 	      AddImport("IOExts");
-	      AddImport("System.Type");
-	      AddImport("System.Enum");
+	      AddImport("Dotnet.System.Type");
+	      AddImport("Dotnet.System.Enum");
 	      sb.AppendFormat("to{0} :: {0}Ty -> {0} (){1}", m_type.Name, System.Environment.NewLine);
 	      sb.AppendFormat("to{0} tag = IOExts.unsafePerformIO (System.Enum.parse (IOExts.unsafePerformIO (System.Type.getType \"{1}\")) (show tag)){2}", m_type.Name, m_type.AssemblyQualifiedName,System.Environment.NewLine);
 	      sb.Append(System.Environment.NewLine);
