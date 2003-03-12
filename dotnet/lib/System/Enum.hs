d23626ed072f18425026e72c862da1990710460b
sof <unknown>
2003-03-12 06:35:38 +0000

    added System.Enum.Parse binding

--- dotnet/lib/System/Enum.hs
+++ dotnet/lib/System/Enum.hs
@@ -2,7 +2,13 @@ module System.Enum where
 
 import DotNet
 import qualified System.ValueType
+import qualified System.TypeTy
 
 data Enum_ a
 type Enum a = System.ValueType.ValueType (Enum_ a)
+
+foreign import dotnet
+  "static System.Enum.Parse"
+  parse :: System.TypeTy.Type a -> String -> IO (Enum b)
+
 
