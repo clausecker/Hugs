cc7f96d70336c5ebafcfa4e8dedbdd4b9a77c400
ross <unknown>
2003-05-22 10:58:17 +0000

    add type synonym FinalizerPtr a, as per RC 10 of FFI spec.

--- libraries/Hugs/ForeignPtr.hs
+++ libraries/Hugs/ForeignPtr.hs
@@ -1,8 +1,9 @@
 module Hugs.ForeignPtr
         ( 
 	  ForeignPtr             -- abstract, instance of: Eq
-        , newForeignPtr          -- :: Ptr a -> FunPtr (Ptr a -> IO ()) -> IO (ForeignPtr a)
-        , addForeignPtrFinalizer -- :: ForeignPtr a -> FunPtr (Ptr a -> IO ()) -> IO ()
+	, FinalizerPtr
+        , newForeignPtr          -- :: Ptr a -> FinalizerPtr a -> IO (ForeignPtr a)
+        , addForeignPtrFinalizer -- :: ForeignPtr a -> FinalizerPtr a -> IO ()
 	, foreignPtrToPtr	 -- :: ForeignPtr a -> Ptr a
 	, touchForeignPtr        -- :: ForeignPtr a -> IO ()
 	, castForeignPtr	 -- :: ForeignPtr a -> ForeignPtr b
@@ -14,8 +15,9 @@ import Foreign.Ptr		( Ptr, FunPtr )
 
 -- data ForeignPtr a -- defined in Prelude.hs
 
-primitive newForeignPtr :: Ptr a -> FunPtr (Ptr a -> IO ()) -> IO (ForeignPtr a)
-primitive addForeignPtrFinalizer :: ForeignPtr a -> FunPtr (Ptr a -> IO ()) -> IO ()
+type FinalizerPtr a = FunPtr (Ptr a -> IO ())
+primitive newForeignPtr :: Ptr a -> FinalizerPtr a -> IO (ForeignPtr a)
+primitive addForeignPtrFinalizer :: ForeignPtr a -> FinalizerPtr a -> IO ()
 primitive touchForeignPtr :: ForeignPtr a -> IO ()
 primitive foreignPtrToPtr :: ForeignPtr a -> Ptr a
 primitive castForeignPtr "primUnsafeCoerce" :: ForeignPtr a -> ForeignPtr b
