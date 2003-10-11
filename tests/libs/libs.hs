be7f54fe67c32db9e958e4066a30b6fe5687d119
ross <unknown>
2003-10-11 00:45:49 +0000

    split the library list, because it creates too many instances.

--- tests/libs/libs.hs
+++ tests/libs/libs.hs
@@ -74,35 +74,3 @@ import Pretty
 import QuickCheck
 import QuickCheckPoly
 import QuickCheckUtils
-
--- currently copied from hslibs
-
-import Assoc
-import AssocDefaults
-import AssocList
-import PatriciaLoMap
-import Collection
-import CollectionDefaults
-import CollectionUtils
-import LazyPairingHeap
-import LeftistHeap
-import MinHeap
-import SkewHeap
-import SplayHeap
-import TestOrdBag
-import TestOrdSet
-import UnbalancedSet
-import BankersQueue
-import BinaryRandList
-import BraunSeq
-import JoinList
-import ListSeq
-import MyersStack
-import RandList
-import RevSeq
-import Sequence
-import SequenceDefaults
-import SimpleQueue
-import SizedSeq
-import TestSeq
-import EdisonPrelude
