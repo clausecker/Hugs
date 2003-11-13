e5d8fc3203c7cf709380e620081d62a89b64d784
ross <unknown>
2003-11-13 15:23:58 +0000

    remove assumption that Char is marshalled as char

--- tests/ffi/String.hs
+++ tests/ffi/String.hs
@@ -1,18 +1,19 @@
 -- !!! Testing marshalling of strings
 
 import Foreign
-import Exception
+import Foreign.C
+import Control.Exception
 
 tests = do
 
-  putStrLn "\nTesting puts (and withString0)"
-  withString0 "Test successful" puts
+  putStrLn "\nTesting puts (and withCString)"
+  withCString "Test successful" puts
 
-  putStrLn "\nTesting peekArray0"
+  putStrLn "\nTesting peekArray0 (and withString0)"
   s <- withString0 "Test successful" (peekArray0 '\0')
   putStr s
 
 
 withString0 s = bracket (newArray0 '\0' s) free
 
-foreign import ccall puts :: Ptr Char -> IO Int
+foreign import ccall puts :: CString -> IO Int
