8dc32d3abff0137d90d9a99e88f1615349bb7b62
sof <unknown>
2003-11-29 02:20:38 +0000

    Added basic StringBuilder abstraction to better support the dynamic construction of char* values. The following functions are on offer:
    extern StringBuilder* newStringBuilder   Args((unsigned int sz));
    extern void           freeStringBuilder  Args((StringBuilder* b));
    extern Bool           prependString      Args((StringBuilder* b, char* str));
    extern Bool           appendString       Args((StringBuilder* b,  char* str));
    extern Bool           appendStringFormat Args((StringBuilder* b, char* fmt, char* arg));
    extern char*          toString           Args((StringBuilder* b));
    
    See opts.h (and opts.c) for documentation as to what the individual
    APIs does.
    
    Note that these new functions on purpose breaks the codebase
    convention of declaring functions K&R style. Radical stuff.

--- src/strutil.c
+++ src/strutil.c
@@ -65,5 +65,226 @@ String* pName; {
     /* Copy everything after the last '.' to the name string */
     *pName = strCopy(dot+1);
   }
+}
 
+/* Cheap&cheerful expandable strings / StringBuilders.
+ *
+ * Note: I'm willfully breaking the convention of using K&R style
+ * function declarations here. Their time has come and gone.
+ */
+typedef struct StringBuilder {
+    unsigned int len;
+    unsigned int size;
+    char*        buf;
+} StringBuilder;
+
+#define INITIAL_BUILDER_SIZE 200
+
+static Bool expandStringBuilder Args((StringBuilder* b, unsigned int newSz));
+
+/*
+ * Function: newStringBuilder(sz)
+ *
+ * Allocate a new StringBuilder object, giving its
+ * initial buffer size 'sz'. If sz == 0, the initial size
+ * is set to INITIAL_BUILDER_SIZE..
+ *
+ */
+StringBuilder*
+newStringBuilder ( unsigned int sz )
+{
+    StringBuilder* b = (StringBuilder*)malloc(sizeof(StringBuilder));
+    
+    if (!b) return NULL;
+
+    b->size = ( sz == 0 ? INITIAL_BUILDER_SIZE : sz);
+    b->len  = 0;
+    b->buf  = (char*)malloc(sizeof(char)*b->size);
+
+    if (!b->buf) {
+	free(b);
+	return NULL;
+    }
+    *(b->buf) = '\0';
+    return b;
+}
+
+/*
+ * Function: expandStringBuilder(builder, newSz)
+ *
+ * Internal function for expanding a StringBuilder's internal
+ * buffer to 'newSz'. Along with expansion the old contents of
+ * the buffer is copied over. 
+ * 
+ * Returns TRUE if successful.
+ */
+static Bool
+expandStringBuilder(StringBuilder* b, unsigned int newSz)
+{
+    char* buf;
+
+    if (!b) return FALSE;
+    if (b->size > newSz) return FALSE;
+
+    buf = realloc(b->buf, newSz);
+    if (!buf) return FALSE;
+    b->size = newSz;
+    b->buf  = buf;
+
+   return TRUE;
+}
+
+/*
+ * Function: prependString(builder, str)
+ *
+ * The preferred usage mode for StringBuilders is to
+ * concatenate stuff at the back, but prependString()
+ * handles addition at the other end.
+ *
+ * Returns TRUE if successful.
+ */
+Bool
+prependString(StringBuilder* b, char* str)
+{
+    unsigned int len, newLen;
+    
+    if (!b) return FALSE;
+    if (!str) return TRUE;
+
+    len = strlen(str);
+    newLen = b->len + len;
+    if (len==0) return TRUE;
+    
+    if ( newLen >= b->size ) {
+	unsigned int newSz = b->size*2;
+	if ( newSz < newLen ) {
+	    newSz = newLen;
+	}
+	if ( !expandStringBuilder(b, newSz) ) {
+	    return FALSE;
+	}
+    }
+
+    /* shift the buffer down to make room for 'str' */
+    memcpy(b->buf+len, b->buf, b->len+1);
+    memcpy(b->buf, str, len-1);
+    b->len = newLen;
+    
+    return TRUE;
+}
+
+/*
+ * Function: appendString(builder, str)
+ *
+ * Append 'str' to the back of 'builder', expanding its
+ * buffer to accommodate the added string, if needs be.
+ *
+ * Returns TRUE if successful.
+ */
+Bool
+appendString(StringBuilder* b, char* str)
+{
+    unsigned int len, newLen;
+    
+    if (!b)   return FALSE;
+    if (!str) return TRUE;
+
+    len = strlen(str);
+    newLen = b->len + len;
+    if (len==0) return TRUE;
+    
+    if ( newLen >= b->size ) {
+	unsigned int newSz = b->size*2;
+	if ( newSz < newLen ) {
+	    newSz = newLen;
+	}
+	if ( !expandStringBuilder(b, newSz) ) {
+	    return FALSE;
+	}
+    }
+
+    memcpy(b->buf+b->len,str,len+1);
+    b->len = newLen;
+    
+    return TRUE;
+}
+
+/*
+ * Function: appendStringFormat(builder, fmt, arg)
+ *
+ * This function provides an extremely limited form of
+ * sprintf()-style format strings, allowing the substitution
+ * of 'arg' zero or more times via the format string 'fmt':
+ *
+ *   appendStringFormat(builder, "-P%s -Y%s", "foo");
+ *
+ * is equivalent to
+ *
+ *   appendString(builder, "-Pfoo -Yfoo");
+ *
+ * Returns TRUE if successful.
+ */
+Bool
+appendStringFormat(StringBuilder* b, char* fmt, char* arg)
+{
+    char* prev = fmt;
+    char* ptr = fmt;
+    char tmp;
+    
+    if (!b) return FALSE;
+
+    while ( (ptr = strchr(ptr, '%')) ) {
+	/* Append verbatim text from the format string */
+	tmp = *ptr;
+	*ptr = '\0';
+	appendString(b, prev);
+	*ptr = tmp;
+
+	if ( ptr[1] != '%' ) {
+	    /* Not an escaped occurrence of '%' */
+	    switch (ptr[1]) {
+	    case 's': appendString(b, arg); ptr += 2; break;
+	    default:
+		fprintf(stderr,"Unsupported format specifier, %%c; ignoring", ptr[1]);
+		ptr += 2; break;
+	    }
+	} else {
+	    if (ptr[1] != '\0') {
+		ptr += 2;
+	    }
+	}
+	prev = ptr;
+    }
+    appendString(b,prev);
+    return TRUE;
+}
+
+/*
+ * Function: toString(builder)
+ *
+ * Returns a pointer to the builder's internal buffer, i.e.,
+ * a copy is not made (=> the caller isn't passed the
+ * responsibility of freeing the returned string.)
+ *
+ * 
+ */
+char*
+toString(StringBuilder* b)
+{
+    if (!b) return NULL;
+    return (b->buf);
+}
+
+/*
+ * Function: freeStringBuilder(builder)
+ *
+ * Free up a StringBuilder object.
+ */
+void
+freeStringBuilder(StringBuilder* b)
+{
+    if (!b) return;
+    if (b->buf) free(b->buf);
+    b->buf = NULL;
+    free(b);
 }
