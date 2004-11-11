6dd275e4ede58f954484bb35e38de34f4d7a498e
ross <unknown>
2004-11-11 14:54:22 +0000

    merge with fptools/testsuite mod200, which tests the same thing

--- tests/static/mod154.hs
+++ tests/static/mod154.hs
@@ -1,5 +1,9 @@
--- !!! ambiguous re-exportation. (shouldn't fail)
+-- !!! Default export list isn't the same as (module M)
+-- This should succeed, exporting only the local 'sort',
+-- and not being confused by the 'sort' from 'List'.
+-- (Hugs gets this wrong)
+
 module M where 
 
-import Prelude as M
-id x = x
+import List as M
+sort = "foo"
