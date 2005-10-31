33b9d84a8e0d7b577ef584f0afa679012a19d84c
neil <unknown>
2005-10-31 19:38:50 +0000

    First version of uninstaller, does not have support for deleting itself yet

---
+++ src/winhugs/uninstaller/Header.h
@@ -0,0 +1,13 @@
+#define WIN32_MEAN_AND_LEAN
+#include <windows.h>
+
+const int MyMaxPath = MAX_PATH * 2;
+
+typedef DWORD u32;
+typedef BYTE u8;
+typedef UINT uint;
+
+
+void InitCRC();
+u32 GetCRC();
+void CRC(const u8* buf, uint len);
