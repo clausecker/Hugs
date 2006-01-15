307a30fdd91aaeb65cde5098f58a67bbde0a4abc
ross <unknown>
2006-01-15 21:12:46 +0000

    minor fix to last commit

--- RPM.mk
+++ RPM.mk
@@ -74,7 +74,7 @@ $(PACKAGE).tar.gz:
 	$(FIND) $(TARTMP)/hugs98/packages -name "*.ly" -o -name "*.y" | \
 		xargs -l $(HAPPY)
 	cd $(TARTMP)/hugs98; $(DARCS_GET) $(DARCS_CPPHS)
-	$(RM) -r cpphs/_darcs
+	$(RM) -r $(TARTMP)/hugs98/cpphs/_darcs
 	cp $(TARTMP)/hugs98/src/version.c $(TARTMP)
 	cd $(TARTMP)/hugs98/src; sed $(VERSION_SUBSTS) < $(TARTMP)/version.c > $(TARTMP)/hugs98/src/version.c
 # using `make parser.c' would be best, but by default yacc
