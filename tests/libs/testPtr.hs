304d6a46593f862dd6d320c1247aa90e348d1376
panne <unknown>
2007-02-26 12:37:23 +0000

    Make the Ptr test work on 32bit and 64bit architectures

--- tests/libs/testPtr.hs
+++ tests/libs/testPtr.hs
@@ -1,2 +1,16 @@
 -- !!! Testing Ptr arithmetic and shows
 import Foreign.Ptr
+
+testShowAndPlus :: IO ()
+testShowAndPlus = sequence_ [putStrLn (show64 (nullPtr `plusPtr` i)) | i <- [-100..100]]
+
+show64 :: Ptr a -> String
+show64 p =
+   case show p of
+      '0':'x':xs -> '0':'x':pseudoSignExtend xs
+
+pseudoSignExtend :: String -> String
+pseudoSignExtend xs =
+   case length xs of
+      8 -> replicate 8 (head xs) ++ xs
+      16 -> xs
